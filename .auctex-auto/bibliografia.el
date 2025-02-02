66c028284d8a49cc3fafac4279c954f23260e8a0
G Lombardi <contato@gutolombardi.com>
2025-02-02 00:10:41 -0300

    Commit automático

--- .auctex-auto/bibliografia.el
+++ .auctex-auto/bibliografia.el
@@ -7,6 +7,7 @@
     "chaib2024:Luta_de_Paradigmas"
     "jordan94:_o_senhor_caos"
     "harvey13:_para_entender_o_capital"
-    "brody18:_save_cat_writer_novel"))
+    "brody18:_save_cat_writer_novel"
+    "gorodovits12:_bíblia_hebraica"))
  '(or :bibtex :latex))
 
