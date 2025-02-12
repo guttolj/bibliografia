b37c1fd64563381691dd6ac828c0a3b4a1259205
Guttemberg Lombardi <contato@gutolombardi.com.br>
2025-02-11 21:00:36 -0300

    Commit automático

--- .auctex-auto/bibliografia.el
+++ .auctex-auto/bibliografia.el
@@ -5,6 +5,8 @@
  (lambda ()
    (LaTeX-add-bibitems
     "Chaib-2024_luta-paradigmas-luta-classes"
+    "Cunha-2008_nova-gramática-português-contemporâneo"
+    "Cegalla-2008_novíssima-gramática-língua-portuguesa"
     "Jordan-1994_o-senhor-caos"
     "Harvey-2013_para-entender-capital"
     "Brody-2018_save-cat-writers-novel"
